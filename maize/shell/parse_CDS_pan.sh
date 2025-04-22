25b0847e2c9fb3f43b820f44a4b4434b3311fd7d
Carson Andorf <andorfc@iastate.edu>
2025-04-22 13:10:05 -0500

    Add files via upload

---
+++ maize/shell/parse_CDS_pan.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+date                          #optional, prints out timestamp at the start of the job in stdout file
+
+# change to the directory containing the files
+
+# loop through the files in the directory
+for file in $(ls ./data/annotations/)
+do
+    # print the filename
+    echo $file
+    python ./scripts/parse_CDS_pan.py ./data/annotations/$file ./cds_pan/${file}.csv
+done
+
+
+date                          #optional, prints out timestamp when the job ends
+#End of file
